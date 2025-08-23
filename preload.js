335c463256dd81f505364b9c950f091010c0dba6
devarsheecodess <devarsheegaunekar29@gmail.com>
2025-08-23 14:42:53 +0530

    Minor fixes

--- preload.js
+++ preload.js
@@ -4,8 +4,8 @@ contextBridge.exposeInMainWorld("electronAPI", {
 	sendPrompt: (text) => ipcRenderer.invoke("send-prompt", text),
 	startListen: () => ipcRenderer.invoke('start-listen'),
 	stopListen: () => ipcRenderer.invoke('stop-listen'),
-	onSpeechResult: (callback) => ipcRenderer.on('speech-result', (event, data) => callback(data)),
 	captureScreen: () => ipcRenderer.invoke('capture-screen'),
 	deleteContext: () => ipcRenderer.invoke('delete-context'),
+	onSpeechResult: (callback) => ipcRenderer.on('speech-result', (event, data) => callback(data)),
 	setGeminiKey: (key) => ipcRenderer.send('set-gemini-key', key),
 });
